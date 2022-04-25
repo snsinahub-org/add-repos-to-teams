bb2455c6154886ba28617f103100d6fbf58ab0dd
Siavash Namvar <snsinahub@github.com>
2022-04-25 15:52:06 -0400

    add all files

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ async function run() {
     console.log("TEAM NAME: " + core.getInput('team_name'))
     const org = core.getInput('org_name')
     const repos2 = await client.paginate(
-        client.repos.listForOrg({ 
+        client.rest.repos.listForOrg({ 
             org: org,
             per_page: 20
         })
