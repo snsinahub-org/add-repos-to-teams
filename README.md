3fe2d4e406b971705d186c58ae12870e4989a849
Siavash Namvar <snsinahub@github.com>
2022-04-25 14:15:42 -0400

    add all files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Add repos to a team
+
+```YAML
+    - name: add-repos
+      uses: snsinahub-org/add-repos-to-teams
+      with:
+        team_name: blah
+        org_name: github.repository_owner
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ src/index.js
@@ -0,0 +1,61 @@
+const {Octokit} = require('@octokit/rest')
+const {retry} = require('@octokit/plugin-retry')
+const {throttling} = require('@octokit/plugin-throttling')
+
+const _Octokit = Octokit.plugin(retry, throttling)
+const client = new _Octokit({
+    auth: core.GITHUB_TOKEN,
+    throttle: {
+        onRateLimit: (retryAfter, options, octokit) => {
+            octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`)
+            if (options.request.retryCount <= 1) {
+                octokit.log.info(`Retrying after ${retryAfter} seconds!`)
+                return true
+            }
+        },
+        onSecondaryRateLimit: (retryAfter, options, octokit) => {
+            octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`)
+            return true
+        },
+    }
+
+})
+
+(async function main() {
+
+    const _repos = await client.paginate(client.repos.listForOrg, {
+        org: core.org_name,
+        type: 'all',
+        per_page: 100
+    })
+    const repos = _repos.map(repo => repo.name)
+    console.log(JSON.stringify(repos, 2))
+
+    // const _teams = await client.paginate(client.teams.listReposInOrg, {
+    //     org: core.org_name,
+    //     team_slug: '<team_slug>',
+    //     per_page: 100
+    // })
+    // const teams = _teams.map(team => team.name)
+
+    // const filteredRepos = repos.filter(repo => {
+    //     return !teams.includes(repo)
+    // })
+
+    // if (filteredRepos.length === 0) {
+    //     console.log('No new repos to add')
+    //     return
+    // }
+
+    // let i = 1
+    // for (const repo of filteredRepos) {
+    //     console.log(`Adding team to repo #${i++} of ${filteredRepos.length}: ${repo}`)
+    //     await client.teams.addOrUpdateRepoPermissionsInOrg({
+    //         org: core.org_name,
+    //         owner: '<owner>',
+    //         repo: repo,
+    //         team_slug: '<team_slug>',
+    //         permission: 'pull',
+    //     })
+    // }
+})()
